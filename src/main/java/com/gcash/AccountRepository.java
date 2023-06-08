bed8c737be1dee5c09a7ce25203af885d0685c5a
jmzarate09 <joshuazarate09@gmail.com>
2023-06-08 12:45:01 +0800

    Added Java Unit Testing Day1

---
+++ src/main/java/com/gcash/AccountRepository.java
@@ -0,0 +1,55 @@
+package com.gcash;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+public class AccountRepository {
+    private final List<Account> accounts = new ArrayList<>();
+
+    public String createAccount(String name, Double initialBalance) {
+        String id = UUID.randomUUID().toString();
+        Account account = new Account(id, name, initialBalance);
+
+        accounts.add(account);
+
+        return id;
+    }
+
+    public Account getAccount(String id) {
+
+//        accounts
+//                .stream()
+//                .filter(account -> id.equals(account.id()))
+//                .findFirst()
+//                .orElse(null);
+
+        for (Account account : accounts) {
+            if (account.id().equals(id)) {
+                return account;
+            }
+        }
+        return null;
+    }
+
+    public void deleteAccount(String id) {
+
+//        accounts
+//                .stream()
+//                .filter(account -> id.equals(account.id()))
+//                .findFirst()
+//                .ifPresent(accounts::remove);
+
+        for (Account account : accounts) {
+            if (account.id().equals(id)) {
+                accounts.remove(account);
+                return;
+            }
+        }
+    }
+
+    public Integer getNumberOfAccounts() {
+        return accounts.size();
+    }
+}
---
+++ src/test/java/com/gcash/AccountRepositoryTest.java
@@ -0,0 +1,76 @@
+package com.gcash;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class AccountRepositoryTest {
+
+    @Test
+    void successfulAccountCreation() {
+        //Setup
+        AccountRepository repository = new AccountRepository();
+
+        //Kick
+        String accountId = repository.createAccount("josh", 89.9);
+
+        //Verify
+        //this assertions will verify if there is an account inside the accountRepository class and the size inside the class.
+        Assertions.assertEquals(1, repository.getNumberOfAccounts());
+        Assertions.assertEquals("josh", repository.getAccount(accountId).name());
+        Assertions.assertNotNull(accountId);
+    }
+
+    @Test
+    void successfulGetAccount() {
+        //Setup
+        AccountRepository repository = new AccountRepository();
+
+        //Kick
+        String accountId = repository.createAccount("josh", 89.9);
+
+        //Verify
+        Assertions.assertEquals("josh", repository.getAccount(accountId).name());
+        Assertions.assertEquals(89.9, repository.getAccount(accountId).balance());
+        Assertions.assertEquals(null, repository.getAccount("randomId"));
+    }
+
+
+    @Test
+    void successfulDeletion() {
+        //Setup
+        AccountRepository repository  = new AccountRepository();
+        String id = repository.createAccount("josh", 89.9);
+
+        //Kick
+        repository.deleteAccount(id);
+
+        //Verify
+        Assertions.assertEquals(0, repository.getNumberOfAccounts());
+    }
+
+
+    @Test
+    void successfulGetNumberOfAccounts() {
+        //Setup
+        AccountRepository repository = new AccountRepository();
+        String id0 = repository.createAccount("josh", 89.9);
+        String id1 = repository.createAccount("josh", 89.9);
+        String id2 = repository.createAccount("josh", 89.9);
+        String id3 = repository.createAccount("josh", 89.9);
+        String id4 = repository.createAccount("josh", 89.9);
+        String id5 = repository.createAccount("josh", 89.9);
+
+
+        //Verify
+        Assertions.assertEquals(6, repository.getNumberOfAccounts());
+
+        //Setup
+        repository.deleteAccount(id0);
+
+        //Verify
+        Assertions.assertEquals(5, repository.getNumberOfAccounts());
+    }
+
+}
+
+
